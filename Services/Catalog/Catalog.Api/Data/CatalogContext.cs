ecb1be127a7636c22a42cdc4b87e0a237ca98ec5
Hojjat Zangeneh <hojjat.zangeneh@gmail.com>
2023-04-11 14:33:01 +0330

    Create MongoDb && Product Repositoriy

---
+++ Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseStrings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseStrings:DatabaseName"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseStrings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.Api/Repositories/IProductsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Api.Entities;
+
+namespace Catalog.Api.Repositories
+{
+    public interface ICatalogRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<Product> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductsByCategory(string category);
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
