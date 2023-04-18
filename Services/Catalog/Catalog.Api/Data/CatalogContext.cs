c0fa1f925c4c3fb3fb1e11489f7fdabc2afcf746
Hojjat Zangeneh <hojjat.zangeneh@gmail.com>
2023-04-18 14:39:03 +0330

    .

--- Services/Catalog/Catalog.Api/Data/CatalogContext.cs
+++ Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -8,6 +8,7 @@ namespace Catalog.Api.Data
         public IMongoCollection<Product> Products { get; }
         public CatalogContext(IConfiguration configuration)
         {
+            var sdsdfsd = configuration.GetValue<string>("DatabaseStrings:ConnectionString");
             var client = new MongoClient(configuration.GetValue<string>("DatabaseStrings:ConnectionString"));
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseStrings:DatabaseName"));
             Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseStrings:CollectionName"));
