f8dbd884b62962188d3ecccff0453cc8e921b3ff
hojjatzangeneh <hojjat.zangeneh@outlook.com>
2023-05-05 09:20:11 +0330

    Update NotFoundException.cs

--- Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
+++ Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Ordering.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace Ordering.Application.Exceptions
 {
     public class NotFoundException : ApplicationException
     {
+        public NotFoundException(string name,object key):base($"Entity {name} and key {key} was not found")
+        {
+            
+        }
     }
 }
