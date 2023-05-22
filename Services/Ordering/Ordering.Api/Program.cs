686b2892c936300e8410bbcdb7ad3e6b7df23690
Hojjat Zangeneh <hojjat.zangeneh@gmail.com>
2023-05-22 15:23:58 +0330

    The End Project and Run All Project On Docker

--- Services/Ordering/Ordering.Api/Program.cs
+++ Services/Ordering/Ordering.Api/Program.cs
@@ -13,7 +13,10 @@ builder.Services.AddInfrastructureServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddHttpLogging(httpLogging =>
+{
+    httpLogging.LoggingFields= Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.All;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,7 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseHttpLogging();
 app.UseAuthorization();
 
 app.MapControllers();
