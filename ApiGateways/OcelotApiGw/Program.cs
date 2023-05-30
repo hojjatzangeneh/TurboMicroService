2abad4ebc175166f17b1823d9571b2d2f35b81d4
Hojjat Zangeneh <hojjat.zangeneh@gmail.com>
2023-05-30 15:35:14 +0330

    End

--- ApiGateways/OcelotApiGw/Program.cs
+++ ApiGateways/OcelotApiGw/Program.cs
@@ -2,13 +2,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Logging.ClearProviders().AddConsole().AddDebug();
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddJsonFile("ocelot.local.json",optional:false,reloadOnChange:true).AddEnvironmentVariables();
 builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 await app.UseOcelot();
-
 app.Run();
